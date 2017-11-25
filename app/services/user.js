7d34710b0e2e6698fd330a1d41897e56ac34fe97
Blake Hagen <blakehagen@gmail.com>
2017-11-25 15:02:03 -0700

    more cleanup

--- app/services/user.js
+++ app/services/user.js
@@ -13,14 +13,6 @@ export default {
     });
   },
 
-  // getUser(userId) {
-  //   return axios.get(`${BASE_URL}user/${userId}`, {
-  //     headers: {
-  //       Authorization: `Bearer ${localStorage.getItem('token')}`,
-  //     },
-  //   });
-  // },
-
   register(registerInfo) {
     return axios.post(`${BASE_URL}signup`, {
       firstName: registerInfo.firstName,
