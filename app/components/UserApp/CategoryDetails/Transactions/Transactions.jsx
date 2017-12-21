2c9d3cbd5faf5a7102a52899475fd0ef3d596bdd
Blake Hagen <blakehagen@gmail.com>
2017-12-21 12:43:49 -0700

    add delete transaction alert

--- app/components/UserApp/CategoryDetails/Transactions/Transactions.jsx
+++ app/components/UserApp/CategoryDetails/Transactions/Transactions.jsx
@@ -5,55 +5,55 @@ import PropTypes from 'prop-types';
 import styles from './transactions.scss';
 
 const Transactions = (props) => {
-  const { transactions, deleteTransaction } = props;
+  const { transactions, openDeleteModal } = props;
   const transactionDetails = _.map(transactions, ({
     id,
     vendor,
     amount,
     description,
     postedDate,
-  }) => {
-    return (
-      <div
-        className={styles.singleTransactionContainer}
-        key={id}
-      >
-        <div className={styles.amountContainer}>
-          {numeral(amount).format('$0,0.00')}
-          <div className={styles.actionContainer}>
+  }) => (
+    <div
+      className={styles.singleTransactionContainer}
+      key={id}
+    >
+      <div className={styles.amountContainer}>
+        {numeral(amount).format('$0,0.00')}
+        <div className={styles.actionContainer}>
 
-            <div className={styles.singleAction}>
-              <div className={styles.actionText}>Edit</div>
-              <div className={styles.editTransactionIcon} />
+          <div
+            className={styles.singleAction}
+          >
+            <div className={styles.actionText}>Edit</div>
+            <div className={styles.editTransactionIcon} />
+          </div>
+
+          <div
+            className={styles.singleAction}
+            onClick={() => openDeleteModal(id, amount)}
+          >
+            <div className={styles.actionText}>
+              Delete
             </div>
+            <div className={styles.deleteTransactionIcon} />
+          </div>
 
-            <div
-              className={styles.singleAction}
-              onClick={() => deleteTransaction(id, amount)}
-            >
-              <div className={styles.actionText}>
-                Delete
-              </div>
-              <div className={styles.deleteTransactionIcon} />
-            </div>
-
-          </div>
-        </div>
-
-        <div className={styles.transactionDetailsContainer}>
-          <div className={styles.row}>
-            <span className={styles.date}>{postedDate}</span>
-          </div>
-          <div className={styles.row}>
-            {vendor}
-          </div>
-          <div className={styles.row}>
-            <span className={styles.description}>{description}</span>
-          </div>
         </div>
       </div>
-    );
-  });
+
+      <div className={styles.transactionDetailsContainer}>
+        <div className={styles.row}>
+          <span className={styles.date}>{postedDate}</span>
+        </div>
+        <div className={styles.row}>
+          {vendor}
+        </div>
+        <div className={styles.row}>
+          <span className={styles.description}>{description}</span>
+        </div>
+      </div>
+    </div>
+  ));
 
   return (
     <div className={styles.transactionWrapper}>
@@ -64,6 +64,7 @@ const Transactions = (props) => {
 
 Transactions.propTypes = {
   transactions: PropTypes.array.isRequired,
+  openDeleteModal: PropTypes.func.isRequired,
 };
 
 export default Transactions;
